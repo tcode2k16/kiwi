c8da104edf4186ff112854eeec925b6d30d4b41e
tcode2k16 <alanc@macbook.local>
2017-01-01 15:53:36 +0800

    building practice view

--- src/components/set_views/Practice.vue
+++ src/components/set_views/Practice.vue
@@ -1,12 +1,12 @@
 <template>
 <div class="component">
-    <el-row type="flex" :getter="progress.total" class="p_bar">
-        <el-col :span="progress.right" class="p_right"></el-col>
-        <el-col :span="progress.wrong" class="p_wrong"></el-col>
-        <el-col :span="progress.total -progress.right -progress.wrong" class="p_none"></el-col>
-    </el-row>
+    <div class="p_bar">
+        <!--<span :style="'width:100%'" class="p_right"></span>
+        <span :span="progress.wrong" class="p_wrong"></span>
+        <span :span="total -progress.right -progress.wrong" class="p_none"></span>-->
+    </div>
 
-    <el-row>
+    <!--<el-row>
         <el-col :span="12">
             <el-button @click="progress.right++">
                 right
@@ -18,11 +18,38 @@
             </el-button>
         </el-col>
     </el-row>
+    <br>-->
+
+    <br>
+    <br>
     <br>
     <el-row type="flex">
         <el-col :span="largeScreen ? 18 : 24" :offset="largeScreen ? 3 : 0">
             <el-card>
-                <h1>hello</h1>
+                <el-row>
+                    <el-col :span="24" class="title">
+                        <p class="text">{{word}}</p>
+                    </el-col>
+                </el-row>
+                <br>
+                <hr class="divider">
+                <br>
+                <el-row type="flex" align="middle">
+                    <el-col :span="18">
+                        <input rows="1"
+                            class="word"
+                            v-model="res"
+                            ref="res"
+                            autocomplete="off"
+                            placeholder="Answer"
+                            autofocus>
+                        </input>
+                        <span class="des" @click="focus_text">TYPE THE ANSWER</span>
+                    </el-col>
+                    <el-col :span="6">
+                        <el-button type="primary">Answer</el-button>
+                    </el-col>
+                </el-row>
             </el-card>
         </el-col>
     </el-row>
@@ -40,8 +67,21 @@ export default {
             progress: {
                 right: 5,
                 wrong: 7,
-                total: 30
-            }
+                
+            },
+            word: "abc",
+            res: 'abc'
+        }
+    },
+    computed: {
+        total() {
+            return Object.keys(this.$store.state.study_sets.sets[this.uuid].set).length
+        }
+    },
+    methods: {
+        focus_text() {
+            console.log('called');
+            this.$refs.res.focus()
         }
     }
 }
@@ -54,18 +94,87 @@ export default {
 .p_bar {
     height: 0.5vh;
     /*border-bottom: 0.5vh solid #fff;*/
+    width: 100%;
 }
 
 .p_none {
-    background-color: #3498db;
+    background-color: #fff;
+    height:100%;
+    display: block;
+    position: relative;
+    overflow: hidden;
 }
 
 .p_right {
     background-color: #2ecc71;
+    height:100%;
+    display: block;
+    position: relative;
+    overflow: hidden;
 }
 
 .p_wrong {
     background-color: #e74c3c;
+    height:100%;
+    display: block;
+    position: relative;
+    overflow: hidden;
 }
 
+.divider {
+    background-color: #f0f0f0;
+    height: 2px;
+    border: none;
+    width: 95%
+}
+
+.word {
+    -webkit-transition: all 0.30s ease-in-out;
+    -moz-transition: all 0.30s ease-in-out;
+    -ms-transition: all 0.30s ease-in-out;
+    -o-transition: all 0.30s ease-in-out;
+    transition:all 0.30s ease-in-out;
+    outline: none;
+    text-align: left;
+    padding: 3px 0px 3px 3px;
+    /*padding: 0 2rem;*/
+    /*margin: 5px 1px 3px 0px;*/
+    margin: 5px auto 1px;
+    border-style: none;
+    border-bottom: 3px solid #475669;
+    font-size: 18px;
+    color: #2c3e50;
+    width: 90%;
+    resize: none;
+    overflow:hidden;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+
+}
+
+.word:focus {
+    border-bottom: 3px solid #20a0ff;
+}
+
+
+.text {
+    text-align: left;
+    /*padding: 3px 0px 3px 3px;*/
+    /*margin: 5px 1px 3px 0px;*/
+    margin: 0 auto;
+    width: 90%;
+    color: #475669;
+    font-size: 30px
+}
+
+
+.des {
+    display: block;
+    text-align: left;
+    margin: 0px auto 1px;
+    width: 90%;
+    color: #97a5aa;
+    font-size: 12px;
+    font-weight: bold;
+    cursor: pointer;
+}
 </style>
